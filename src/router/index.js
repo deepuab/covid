1e6f58499e242afe5422a02c58d1a55fa616a0a8
deepuab <deepu.xguy@gmail.com>
2020-08-05 10:07:04 +0530

    Disabled HTML5 mode

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ import DistrictDetailsPage from '../districtDetails1/DistrictDetailsPage.vue';
 Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
+    // mode: 'history',
     routes: [{
         path: '/',
         name: 'Home',
